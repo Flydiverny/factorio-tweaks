c7013f35421a53f3be447581d869e6e9aa09da8f
Markus Maga <markus@maga.se>
2022-10-05 22:42:07 +0200

    feat: allow ducts in space
    Signed-off-by: Markus Maga <markus@maga.se>

---
+++ data.lua
@@ -0,0 +1,28 @@
+local ducts = {
+  ["pipe-to-ground"] = { "duct-underground" },
+  ["pump"] = {
+    "duct-end-point-intake",
+    "duct-end-point-outtake",
+    "non-return-duct",
+  },
+  ["storage-tank"] = {
+    "duct-cross",
+    "duct-curve",
+    "duct-long",
+    "duct-small",
+    "duct-t-junction",
+    "duct",
+  },
+}
+
+local function allow_in_space(entity_map)
+  for entity_type, entities in pairs(allow_in_space) do
+    for entity in pairs(entities) do
+      data[entity_type][entity].se_allow_in_space = true
+    end
+  end
+end
+
+if mods["FluidMustFlow"] then
+  allow_in_space(ducts)
+end
